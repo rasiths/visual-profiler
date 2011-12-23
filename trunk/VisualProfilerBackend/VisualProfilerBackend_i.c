cbb5c1fad0be31e2b47bf49855d946f7e1bfdfee
jan.vratislav@gmail.com <jan.vratislav@gmail.com@5e1a5f06-61d4-829d-86ff-a830b0f65a3c>
2011-12-23 23:06:25 +0000

    References fixed

--- trunk/VisualProfilerBackend/VisualProfilerBackend_i.c
+++ trunk/VisualProfilerBackend/VisualProfilerBackend_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Nov 30 20:57:12 2011
+/* at Fri Dec 23 23:58:49 2011
  */
 /* Compiler settings for VisualProfilerBackend.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
